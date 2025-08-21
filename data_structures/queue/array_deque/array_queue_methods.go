777354c4c50367594b7f38d3a2b758cd3216d4e2
WolfieLeader <koren2318@gmail.com>
2025-08-21 13:13:27 +0300

    fix queue, deque, and stack

--- data_structures/queue/array_deque/array_queue_methods.go
+++ data_structures/queue/array_deque/array_queue_methods.go
@@ -7,7 +7,7 @@ func (d *ArrayDeque[T]) DequeueLast() (T, bool)   { return d.data.Delete(d.data.
 func (d ArrayDeque[T]) PeekFirst() (T, bool)      { return d.data.Get(0) }
 func (d ArrayDeque[T]) PeekLast() (T, bool)       { return d.data.Get(d.data.Length() - 1) }
 func (d ArrayDeque[T]) Size() int                 { return d.data.Length() }
-func (d ArrayDeque[T]) IsEmpty() bool             { return d.data.Length() == 0 }
+func (d ArrayDeque[T]) IsEmpty() bool             { return d.data.IsEmpty() }
 func (d *ArrayDeque[T]) Clear()                   { d.data.Clear() }
-func (d ArrayDeque[T]) ToSlice() []T              { return d.data.Copy() }
+func (d ArrayDeque[T]) ToSlice() []T              { return d.data.ToSlice() }
 func (d ArrayDeque[T]) Copy() *ArrayDeque[T]      { return &ArrayDeque[T]{data: d.data.Copy()} }
--- data_structures/stack/array_stack/array_stack_methods.go
+++ data_structures/stack/array_stack/array_stack_methods.go
@@ -6,5 +6,5 @@ func (s *ArrayStack[T]) Peek() (T, bool)      { return s.data.Get(s.data.Length(
 func (s *ArrayStack[T]) Size() int            { return s.data.Length() }
 func (s *ArrayStack[T]) IsEmpty() bool        { return s.data.IsEmpty() }
 func (s *ArrayStack[T]) Clear()               { s.data.Clear() }
-func (s *ArrayStack[T]) ToSlice() []T         { return s.data.Reverse() }
+func (s *ArrayStack[T]) ToSlice() []T         { return s.data.Reverse().ToSlice() }
 func (s *ArrayStack[T]) Copy() *ArrayStack[T] { return &ArrayStack[T]{data: s.data.Copy()} }
