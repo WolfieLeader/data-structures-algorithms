73c7771af317b7c30a37124e9afdf9f62fcbf088
WolfieLeader <koren2318@gmail.com>
2025-09-12 15:41:41 +0300

    implement interface

--- data_structures/tree/heap/heap_interface.go
+++ data_structures/tree/heap/heap_interface.go
@@ -8,7 +8,11 @@ type heap[T any] interface {
 	Pop() (T, bool)
 	Push(values ...T)
 	Contain(value T) bool
+	Heapify(values ...T)
+	ToSlice() []T
 	Copy() *Heap[T]
 	Equal(other *Heap[T]) bool
 	String() string
 }
+
+var _ heap[int] = (*Heap[int])(nil)
