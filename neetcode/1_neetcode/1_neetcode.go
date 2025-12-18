4645bbf9d40f8a22201503fda8248d3bffa7a4de
WolfieLeader <koren2318@gmail.com>
2025-12-18 17:31:40 +0200

    neetcode

---
+++ neetcode/1_neetcode/1_neetcode.go
@@ -0,0 +1,12 @@
+package main
+
+func hasDuplicate(nums []int) bool {
+	seen := make(map[int]struct{}, len(nums))
+	for _, v := range nums {
+		if _, found := seen[v]; found {
+			return true
+		}
+		seen[v] = struct{}{}
+	}
+	return false
+}
