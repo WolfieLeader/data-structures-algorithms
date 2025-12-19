02ae9f3fb124027c03154f06e29a40e8b745850d
WolfieLeader <koren2318@gmail.com>
2025-12-19 21:01:28 +0200

    tidy + prefix and suffix

---
+++ leetcode/nc_1007_prefix_suffix.go
@@ -0,0 +1,20 @@
+package main
+
+// I: [1,2,4,6] => O: [48,24,12,8]
+// I: [-1,0,1,2,3] => O: [0,-6,0,0]
+
+func productExceptSelf(nums []int) []int {
+	out := make([]int, len(nums))
+	out[0] = 1
+	// Prefix products
+	for i := 1; i < len(nums); i++ {
+		out[i] = out[i-1] * nums[i-1]
+	}
+	// Suffix products
+	rightProduct := 1 // like a carry
+	for i := len(nums) - 1; i >= 0; i-- {
+		out[i] *= rightProduct
+		rightProduct *= nums[i]
+	}
+	return out
+}
