b68630397739fa5ff1f5e057635c3f612bc44d41
WolfieLeader <koren2318@gmail.com>
2025-12-19 13:01:39 +0200

    Top k Freq

---
+++ neetcode/5_neetcode/5_neetcode.go
@@ -0,0 +1,24 @@
+package main
+
+func topKFrequent(nums []int, k int) []int {
+	freqs := make(map[int]int, len(nums)) // Number: Times
+	for _, v := range nums {
+		freqs[v]++
+	}
+
+	buckets := make([][]int, len(nums)+1)
+	for num, freq := range freqs {
+		buckets[freq] = append(buckets[freq], num)
+	}
+
+	out := make([]int, 0, k)
+	for i := len(buckets) - 1; i > 0 && len(out) < k; i-- {
+		for _, num := range buckets[i] {
+			out = append(out, num)
+			if len(out) == k {
+				break
+			}
+		}
+	}
+	return out
+}
