9d17a1d90e812fdfcd3d7f60053f2734472080ce
WolfieLeader <koren2318@gmail.com>
2025-12-20 01:52:47 +0200

    Two pointers Slow and Fast

---
+++ leetcode/lc_0026_two_pointers_sf.go
@@ -0,0 +1,18 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	slow := 0
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] == nums[slow] {
+			continue
+		}
+		slow++
+		nums[slow] = nums[fast]
+	}
+
+	return slow + 1
+}
