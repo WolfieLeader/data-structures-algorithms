d4248faee4aee40d7f95c8f745b78ddecc370dfd
WolfieLeader <koren2318@gmail.com>
2025-12-20 13:32:19 +0200

    More Slow Fast Pointers

---
+++ leetcode/lc_0283_two_pointers_sf.go
@@ -0,0 +1,32 @@
+package main
+
+// I: [0,1,0,3,12] => O: [1,3,12,0,0]
+
+func moveZeroes(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] == 0 {
+			continue
+		}
+		nums[slow] = nums[fast]
+		slow++
+	}
+
+	for slow < len(nums) {
+		nums[slow] = 0
+		slow++
+	}
+}
+
+func optimizedMoveZeroes(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] == 0 {
+			continue
+		}
+		if slow != fast {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+		}
+		slow++
+	}
+}
